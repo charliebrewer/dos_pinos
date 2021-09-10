4fed4cf39bfe9620cd81c6e034aed7c2c61c3c89
charlie <cjbrew@gmail.com>
2021-09-10 07:51:00 -0700

    Added image.

--- src/pages/MainSite.tsx
+++ src/pages/MainSite.tsx
@@ -5,14 +5,34 @@ import NavBar from '../components/NavBar';
 import Welcome from './Welcome';
 import Footer from '../components/Footer';
 import { observer } from 'mobx-react-lite';
+import styled, { createGlobalStyle } from 'styled-components';
+import headerImg from '../assets/cropped-header-four-2016.jpg';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f1f1f1;
+  }
+`;
+
+const App = styled.div`
+  max-width: 900px;
+  margin: auto;
+  background-color: #ffffff;
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+`;
 
 export default observer(() => {
   const store = useContext(StoreContext) as Store;
 
   return (
-    <div className="App">
+    <App>
+      <GlobalStyle />
       <header>
         <h1>Dos Pinos Housing Cooperative</h1>
+        <StyledImg src={headerImg} />
         <NavBar></NavBar>
       </header>
       <div>{store.page}</div>
@@ -21,6 +41,6 @@ export default observer(() => {
       </div>
       <Footer></Footer>
       <button onClick={() => store.setPage('other')}>press</button>
-    </div>
+    </App>
   );
 });
