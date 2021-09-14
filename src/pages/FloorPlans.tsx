7e600c830d995ad6029490792aa7c0741fa70af3
charlie <cjbrew@gmail.com>
2021-09-13 18:09:39 -0700

    Updated floorplans.

--- src/pages/FloorPlans.tsx
+++ src/pages/FloorPlans.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import big2br from '../assets/big2br.jpg';
+import big3brDown from '../assets/big3br-downstairs.jpg';
+import big3brUp from '../assets/big3br-upstairs.jpg';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  img {
+    width: 80%;
+    margin-left: 10%;
+  }
+`;
 
 export default () => {
   return (
-    <div>
+    <StyledDiv>
       <h2>Floor Plans</h2>
-    </div>
+
+      <p>
+        <b>Dos Pinos Offers One, Two and Three Bedroom Floor Plans</b>
+      </p>
+
+      <p>
+        (There are 60 total units. Of these 6 are 1-bedroom, 28 are 2-bedroom,
+        and 26 are 3-bedroom. PLEASE NOTE: Very few units currently have wood
+        stoves though these diagrams include them.)
+      </p>
+
+      <p>One Bedroom</p>
+
+      <img src={big2br} />
+
+      <p>Two Bedroom</p>
+
+      <img src={big2br} />
+
+      <p>Three Bedroom (downstairs)</p>
+
+      <img src={big3brDown} />
+
+      <p>Three Bedroom (upstairs)</p>
+
+      <img src={big3brUp} />
+    </StyledDiv>
   );
 };
