10f44dd31adb7744e483aa73f675e8769fa550bd
charlie <cjbrew@gmail.com>
2022-05-29 10:14:22 -0700

    Updated member's handbook.

--- src/pages/MembersHandbook.tsx
+++ src/pages/MembersHandbook.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import membersHandbook from '../assets/Dos_Pinos_Housing_Cooperative_Members_Handbook_August_2021-1.pdf';
+import membersHandbook from '../assets/Dos_Pinos_Housing_Cooperative_Members_Handbook_2022-05-23.pdf';
 
 export default () => {
   return (
@@ -7,13 +7,13 @@ export default () => {
       <h2>Member's Handbook</h2>
 
       <p>
-        The attached PDF copy of the Dos Pinos Housing Cooperative Members’
+        The attached PDF copy of the Dos Pinos Housing Cooperative Members'
         Handbook was updated in August 2021. Documents or policies revised or
         added after that date are not reflected in this copy.
       </p>
 
       <a href={membersHandbook} target="_blank">
-        Dos Pinos Housing Cooperative Members’ Handbook August 2021
+        Dos Pinos Housing Cooperative Members' Handbook May 2022
       </a>
     </div>
   );
